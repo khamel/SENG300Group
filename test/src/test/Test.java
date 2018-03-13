0bef29820ce1f85aa83ce0f26c9af616fe06c330
Sarah Walker <sarah.walker45@gmail.com>
2018-03-13 15:52:17 -0600

    Commit 4 - For test suite - basic comments

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,11 +1,19 @@
 package test;
 
 
+/*	SENG300 Project Iteration 1
+ * 	Sarah Walker, Christian Chabot, Keith Hamel
+ * 
+ * 	Input: 	Path to directory containing java files
+ * 			Fully qualified data type name
+ * 
+ * 	Output:	Number of references and declarations of input data type 
+ * 
+ */
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
@@ -13,7 +21,6 @@ import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
 import org.eclipse.jdt.core.dom.EnumDeclaration;
 
 public class Test {
-	
 	
 	public static String type;
 	public static String inputFilePath;
@@ -24,29 +31,39 @@ public class Test {
 	
 	public static void main(String[] args) 
 	{
-		//TODO
 		getUserInput();
 		String source = convertSourceToString();
-		
 		parseSource(source);
 		printResults();
 	}
-	
+	/*
+	 * 
+	 * 
+	 */
+	//TODO
 	public static void getUserInput()
 	{
 		//TODO - set type in here as well
-		type = "A";
-
+		type = "int";
+		return;
 	}
 	
+	/*
+	 * 
+	 * 
+	 */
+	//TODO
 	public static String convertSourceToString()
 	{
-		//TODO
-		String source = "public class A { int j; \n ArrayList<String> al = null; \n char a = 'a'; \n String n; \n double i;}";
+		String source = "public class A { int j; \n ArrayList<String> al = null; \n char a = 'a'; \n String n; \n double i; \n j =1;}";
 		return source;
 
 	}
 
+	/*
+	 * 
+	 * 
+	 */
 	public static void parseSource(String source)
 	{
 		ASTParser parser = ASTParser.newParser(AST.JLS9);
@@ -59,88 +76,84 @@ public class Test {
 		
 		parser.setUnitName(inputFilePath.toString());
 		
-		String[] classPath = {""};//source class file location
-		String[] sourcePath = {""};//source java file location
+		// TODO - Source Path
+		String[] classPath = {""};				//source class file location
+		String[] sourcePath = {inputFilePath};	//source java file location
 		
 		parser.setEnvironment(classPath, sourcePath, new String[] {"UTF-8"}, true);
 		
 		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 		cu.accept(new ASTVisitor()
 			{
+			/* Primitives
+			 * (non-Javadoc)
+			 * 
+			 * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.VariableDeclarationFragment)
+			 */
+			
 			public boolean visit(VariableDeclarationFragment node)
 				{
 					ITypeBinding binding = node.getName().resolveTypeBinding();
-					
-					//REMOVE
-					System.out.println(binding.getQualifiedName());//
-					
 					if(type.equals(binding.getQualifiedName()))
 					{
-						numDeclarations++;
+						numReferences++;
 					}
-					
 					return true;
 				}
-			
+			/* Classes - nesting
+			 * (non-Javadoc)
+			 * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.TypeDeclaration)
+			 */
 			public boolean visit(TypeDeclaration node) 
 				{
 					ITypeBinding binding = node.getName().resolveTypeBinding();
-					
-					//REMOOVE
-					System.out.println(binding.getQualifiedName());
-					
 					if(type.equals(binding.getQualifiedName()))
-							{
+						{
 								numDeclarations++;
-							}
-					
-				
+						}
 					return true;
 				}
 			
+			/* Annotation - @ stuff
+			 * (non-Javadoc)
+			 * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.AnnotationTypeDeclaration)
+			 */
 			public boolean visit(AnnotationTypeDeclaration node) 
 			{
 				ITypeBinding binding = node.getName().resolveTypeBinding();
-				
-				//REMOOVE
-				System.out.println(binding.getQualifiedName());
-				
 				if(type.equals(binding.getQualifiedName()))
 						{
 							numDeclarations++;
 						}
-				
-			
 				return true;
 			}
+			
+			/* Enum - enum Quark ???
+			 * (non-Javadoc)
+			 * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.EnumDeclaration)
+			 */
 			
 			public boolean visit(EnumDeclaration node) 
 			{
 				ITypeBinding binding = node.getName().resolveTypeBinding();
-				
-				//REMOOVE
-				System.out.println(binding.getQualifiedName());
-				
 				if(type.equals(binding.getQualifiedName()))
 						{
 							numDeclarations++;
 						}
-				
-			
 				return true;
 			}
-			});
-		
+		});
 		
 		return;
 	}
 	
+	/*
+	 * 
+	 * 
+	 */
 	public static void printResults()
 	{
-		//TODO
-		
-		System.out.println("Number of declarations: " + numDeclarations);
-		
+		System.out.println(type + ". Declarations found: " + numDeclarations + "; references found: " + numReferences + ".");
 		return;
 	}
 
