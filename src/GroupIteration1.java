2af79c3fe114d45983359668b56bcf6a1702db51
Sarah Walker <sarah.walker45@gmail.com>
2018-03-13 12:13:18 -0600

    Commit 2 - Basic driver and empty methods

--- src/GroupIteration1.java
+++ src/GroupIteration1.java
@@ -1,4 +1,44 @@
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+
 public class GroupIteration1 {
 
+	public static String source;
+	public static String type;
+	
+	public static int declarations;
+	public static int references;
+	
+	
+	public static void main(String[] args) 
+	{
+		//TODO
+		getUserInput();
+		
+		parseSource();
+		
+		printResults();
+	}
+	
+	public static void getUserInput()
+	{
+		//TODO
+		return;
+	}
+	
+	public static void printResults()
+	{
+		//TODO
+		return;
+	}
+	
+	public static void parseSource()
+	{
+		//TODO
+		return;
+	}
+	
 }
